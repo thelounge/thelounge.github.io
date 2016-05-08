ac1a69c9127dab55d38876b98321be3712a27973
Jérémie Astori <jeremie@astori.fr>
2016-05-08 15:42:05 -0400

    Use querySelector instead of getElementById

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 	$.getJSON("https://api.github.com/repos/thelounge/lounge/tags", function(json) {
 		var first = json.shift();
 		if (first.name) {
-			var version = document.getElementById("version");
+			var version = document.querySelector("#version");
 			version.textContent = "version " + first.name.substr(1); // Strip `v` in `vX.Y.Z`
 			version.className = "version_shown";
 		}
