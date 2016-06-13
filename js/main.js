6f9c0db9541868407fb38a3b734d3a7e6a3ab0c8
Jérémie Astori <jeremie@astori.fr>
2016-06-13 00:21:30 -0400

    Use GitHub releases to retrieve latest non-prerelease version

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 (function() {
-	$.getJSON("https://api.github.com/repos/thelounge/lounge/tags", function(json) {
-		var first = json.shift();
-		if (first.name) {
+	$.getJSON("https://api.github.com/repos/thelounge/lounge/releases", function(json) {
+		var first = json.filter(function (release) {
+			return !release.prerelease;
+		})[0];
+
+		if (first.tag_name) {
 			var version = document.getElementById("version");
-			version.textContent = "version " + first.name.substr(1); // Strip `v` in `vX.Y.Z`
+			version.textContent = "version " + first.tag_name.substr(1); // Strip `v` in `vX.Y.Z`
 			version.className = "version_shown";
 		}
 	});
