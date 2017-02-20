430a4ec8935694ff809e295a3cf1439c9bfd7823
Jérémie Astori <jeremie@astori.fr>
2017-02-20 02:58:30 -0500

    Search cleanup

--- js/main.js
+++ js/main.js
@@ -23,35 +23,33 @@
 	}
 
 	$("#search-btn").click(function(event) {
-		if (!$("#search-input").hasClass("active")) {
-			$(".search > div").addClass("active");
-			$(".search > div").one("transitionend webkitTransitionEnd oTransitionEnd", function () {
-				console.log('transitionend');
+		$(".nav-search-overlay")
+			.addClass("opened")
+			.one("transitionend webkitTransitionEnd oTransitionEnd", function () {
 				$("#search-input").focus();
 			});
-			event.preventDefault();
-		}
-	});
-
-	$("#cancel-btn").click(function(event) {
-		$(".search > div").removeClass("active");
-		$(".search > div").one("transitionend webkitTransitionEnd oTransitionEnd", function () {
-			$("#search-input").val("");
-			$("#search-input").trigger("keyup");
-		});
-
 		event.preventDefault();
 	});
 
-	$(document).keypress(function (e) {
-		if (e.which === 115) { // "S" key
+	$("#search-close-btn").click(function(event) {
+		$(".nav-search-overlay")
+			.removeClass("opened")
+			.one("transitionend webkitTransitionEnd oTransitionEnd", function () {
+				$("#search-input").val("").trigger("keyup");
+			});
+		event.preventDefault();
+	});
+
+	$(document).keypress(function (event) {
+		if (!$(event.target).is('input') && event.which === 115) { // "S" key
 			$("#search-btn").click();
 		}
 	});
 
-	$("#search-input").keydown(function (e) {
-		if (e.which === 27) { // "Escape" key
-			$("#cancel-btn").click();
+	$("#search-input").keydown(function (event) {
+		if (event.which === 27) { // "Escape" key
+			$("#search-close-btn").click();
+			event.preventDefault();
 		}
 	});
 }());
