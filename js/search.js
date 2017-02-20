04fb4b3fac9c0fd08c382a3598d6653b85c0331e
Jérémie Astori <jeremie@astori.fr>
2017-02-20 02:58:30 -0500

    Search cleanup

--- js/search.js
+++ js/search.js
@@ -1,68 +1,52 @@
 (function() {
-  function displaySearchResults(results, store) {
-    var searchResults = document.getElementById('search-results');
+	function displaySearchResults(results, store) {
+		var searchResults = $("#search-results");
+		var resultContent = '<div class="container"><h1>Search results</h1>';
 
-    $("#search-results").show();
+		if (results.length) { // Are there any results?
+			resultContent += "<ul>";
+			results.forEach(function (result) {
+				var item = store[result.ref];
+				resultContent += '<li><a href="' + item.url + '"><h3>' + item.title + '</h3></a>';
+			});
+			resultContent += "</ul>";
+		} else {
+			resultContent += "<p>No results found</p>";
+		}
+		resultContent += "</div>";
+		searchResults.html(resultContent);
+		searchResults.show();
+	}
 
-    if (results.length) { // Are there any results?
-      var appendString = '<div class="container"><h1>Search results</h1><ul>';
+	$("#search-input").keyup(function() {
+		var searchTerm = $(this).val();
 
-      for (var i = 0; i < results.length; i++) {  // Iterate over the results
-        var item = store[results[i].ref];
-        appendString += '<li><a href="' + item.url + '"><h3>' + item.title + '</h3></a>';
-      }
+		if (searchTerm) {
+			$("#content").hide();
 
-      appendString += "</ul></div>";
+			// Initalize lunr with the fields it will be searching on. I've given title
+			// a boost of 10 to indicate matches on this field are more important.
+			var idx = lunr(function () {
+				this.field('id');
+				this.field('title', { boost: 10 });
+				this.field('category');
+				this.field('content', { boost: 5 });
+			});
 
-      searchResults.innerHTML = appendString;
-    } else {
-      searchResults.innerHTML = '<div class="container"><h1>Search results</h1><p>No results found</p></div>';
-    }
-  }
+			for (var key in window.store) { // Add the data to lunr
+				idx.add({
+					'id': key,
+					'title': window.store[key].title,
+					'category': window.store[key].category,
+					'content': window.store[key].content
+				});
 
-  function getQueryVariable(variable) {
-    var query = window.location.search.substring(1);
-    var vars = query.split('&');
-
-    for (var i = 0; i < vars.length; i++) {
-      var pair = vars[i].split('=');
-
-      if (pair[0] === variable) {
-        return decodeURIComponent(pair[1].replace(/\+/g, '%20'));
-      }
-    }
-  }
-
-  $("#search-input").keyup(function() {
-    var searchTerm = $(this).val();
-
-    if (searchTerm) {
-      $("#content").hide();
-      // document.getElementById('search-input').setAttribute("value", searchTerm);
-
-      // Initalize lunr with the fields it will be searching on. I've given title
-      // a boost of 10 to indicate matches on this field are more important.
-      var idx = lunr(function () {
-        this.field('id');
-        this.field('title', { boost: 10 });
-        this.field('category');
-        this.field('content', { boost: 5 });
-      });
-
-      for (var key in window.store) { // Add the data to lunr
-        idx.add({
-          'id': key,
-          'title': window.store[key].title,
-          'category': window.store[key].category,
-          'content': window.store[key].content
-        });
-
-        var results = idx.search(searchTerm); // Get lunr to perform a search
-        displaySearchResults(results, window.store); // We'll write this in the next section
-      }
-    } else {
-      $("#search-results").hide();
-      $("#content").show();
-    }
-  });
+				var results = idx.search(searchTerm); // Get lunr to perform a search
+				displaySearchResults(results, window.store); // We'll write this in the next section
+			}
+		} else {
+			$("#search-results").hide();
+			$("#content").show();
+		}
+	});
 })();
