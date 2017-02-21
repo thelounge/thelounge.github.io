7dae6e25a096278dd3a05feed27c4a0306c71d43
Jérémie Astori <jeremie@astori.fr>
2017-02-21 01:00:49 -0500

    Move version detection to default layout to load jQuery in all pages

--- js/main.js
+++ js/main.js
@@ -1,24 +1,24 @@
 (function() {
-	$.getJSON("https://api.github.com/repos/thelounge/lounge/releases", function(json) {
-		var latest = json[0];
-		var stable;
-		var prerelease;
+	if ($("#stable_version").length) {
+		$.getJSON("https://api.github.com/repos/thelounge/lounge/releases", function(json) {
+			var latest = json[0];
+			var stable;
+			var prerelease;
 
-		if (latest.prerelease) {
-			prerelease = latest;
-			stable = json.find(function (release) { return !release.prerelease; });
-		} else {
-			stable = latest;
-		}
+			if (latest.prerelease) {
+				prerelease = latest;
+				stable = json.find(function (release) { return !release.prerelease; });
+			} else {
+				stable = latest;
+			}
 
-		var stable_version = document.getElementById("stable_version");
-		// `.substr(1)` strips `v` in `vX.Y.Z`
-		stable_version.textContent = "version " + stable.tag_name + (prerelease ? " (stable)" : "");
+			// `.substr(1)` strips `v` in `vX.Y.Z`
+			$("#stable_version").text("version " + stable.tag_name + (prerelease ? " (stable)" : ""));
 
-		if (prerelease) {
-			var prerelease_version = document.getElementById("prerelease_version");
-			prerelease_version.textContent = "version " + prerelease.tag_name.substr(1) + " (pre-release)";
-			document.getElementById("prerelease_card").className = "card";
-		}
-	});
+			if (prerelease) {
+				$("#prerelease_version").text("version " + prerelease.tag_name.substr(1) + " (pre-release)");
+				$("#prerelease_card").show();
+			}
+		});
+	}
 }());
