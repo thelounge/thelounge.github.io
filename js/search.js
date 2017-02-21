e0491c0a0a734d11bb05dd0c9067292de27a710c
Jérémie Astori <jeremie@astori.fr>
2017-02-20 21:11:35 -0500

    Improve perf of instant search

--- js/search.js
+++ js/search.js
@@ -1,56 +1,129 @@
 (function() {
-	function displaySearchResults(results, store) {
-		var searchResults = $("#search-results");
-		var resultContent = '<div class="container"><h1>Search results</h1>';
-
-		if (results.length) { // Are there any results?
-			resultContent += "<ul>";
-			results.forEach(function (result, index) {
-				var item = store[result.ref];
-				resultContent += '<li ' + (index === 0 ? 'class="selected"': '') + '><a href="' + item.url + '">' + item.title + '</a>';
-			});
-			resultContent += "</ul>";
-		} else {
-			resultContent += "<p>No results found</p>";
-		}
-		resultContent += "</div>";
-		searchResults.html(resultContent);
-		searchResults.show();
+	// Preparing lunrjs index based on store located in HTML pages
+	var lunrIndex = lunr(function () {
+		this.field('id');
+		this.field('title', { boost: 10 });
+		this.field('category');
+		this.field('content', { boost: 5 });
+	});
+	for (var key in window.store) {
+		lunrIndex.add({
+			'id': key,
+			'title': window.store[key].title,
+			'category': window.store[key].category,
+			'content': window.store[key].content
+		});
 	}
 
-	$("#search-input").keyup(function(event) {
-		if (event.which === 38 || event.which === 40 || event.which === 13) {
-			return;
-		}
-
+	// Run the search every time the search input value changes.
+	// Use "input" event instead of `keydown`/`keypress`/`keyup` to focus on
+	// actual content change.
+	$("#search-input").on('input', function(event) {
 		var searchTerm = $(this).val();
+		var resultsElement = $("#search-results");
+		var contentElement = $("#content");
 
+		// When the search field is not empty, run lunrjs and wrap all results in a
+		// list or report that no results were found.
+		// The first result is marked as "selected" so up/down arrows can cycle
+		// through results while pressing Enter will click on the link.
 		if (searchTerm) {
-			$("#content").hide();
+			var results = lunrIndex.search(searchTerm);
+			var resultsContent;
 
-			// Initalize lunr with the fields it will be searching on. I've given title
-			// a boost of 10 to indicate matches on this field are more important.
-			var idx = lunr(function () {
-				this.field('id');
-				this.field('title', { boost: 10 });
-				this.field('category');
-				this.field('content', { boost: 5 });
-			});
-
-			for (var key in window.store) { // Add the data to lunr
-				idx.add({
-					'id': key,
-					'title': window.store[key].title,
-					'category': window.store[key].category,
-					'content': window.store[key].content
+			if (results.length) {
+				resultsContent = "<ul>";
+				results.forEach(function (result, index) {
+					var url = result.ref;
+					var item = window.store[url];
+					resultsContent += '<li ' + (index === 0 ? 'class="selected"': '') + '>';
+					resultsContent += '<a href="' + url + '">' + item.title + '</a>';
+					resultsContent += "</li>";
 				});
-
-				var results = idx.search(searchTerm); // Get lunr to perform a search
-				displaySearchResults(results, window.store); // We'll write this in the next section
+				resultsContent += "</ul>";
+			} else {
+				resultsContent = "<p>No results found.</p>";
 			}
+			$("#results-placeholder").html(resultsContent);
+			resultsElement.show();
+			contentElement.hide();
 		} else {
-			$("#search-results").hide();
-			$("#content").show();
+			// When the search input is empty, hide the result page and display the
+			// current page.
+			resultsElement.hide();
+			contentElement.show();
 		}
+	});
+
+	// This must be `keydown` to override built-in handlers. `keypress` is not
+	// emitted on non-printable characters (also differs across browsers) and
+	// `keyup` arrives too late.
+	$(document).keydown(function (event) {
+		if (event.which === 83 && !$(event.target).is('input')) {
+			// "S" key and not currently on an input, to avoid swallowing it.
+			$("#search-btn").click();
+			return false;
+		} else if (event.which === 40) {
+			// "Down" key, cycle downward through the search results.
+			var selected = $("#search-results .selected");
+			if (selected.next().length) {
+				selected.removeClass("selected");
+				selected.next().addClass("selected");
+			}
+			return false;
+		} else if (event.which === 38) {
+			// "Up" key, cycle upward through the search results.
+			var selected = $("#search-results .selected");
+			if (selected.prev().length) {
+				selected.removeClass("selected");
+				selected.prev().addClass("selected");
+			}
+			return false;
+		} else if (event.which === 13) {
+			// "Enter" key, visit the currently selected result. Must use
+			// `querySelector` instead of jQuery as jQuery's `click` will trigger
+			// click event handlers instead of actually clicking on the link.
+			document.querySelector("#search-results .selected a").click();
+			return false;
+		} else if (event.which === 27) {
+			// "Escape" key, cancels current search. This cannot be on keyup as inputs
+			// are being cleared by default on Escape keydown.
+			$("#search-close-btn").click();
+			return false;
+		}
+	});
+
+	$("#search-input").keydown(function (event) {
+		if (event.which === 27) {
+			// Do not clear input when pressing "Escape" but let it bubble up to
+			// `document.keydown` handler.
+			event.preventDefault();
+		}
+	});
+
+	$("#search-btn").click(function() {
+		$(".nav-search-overlay")
+			.addClass("opened")
+			.one("transitionend webkitTransitionEnd oTransitionEnd", function () {
+				// `.focus()` does not work on hidden elements, which also happens if
+				// they are starting their transition.
+				$("#search-input").focus();
+			});
+		// This is required to focus the input when there is no transition, i.e.
+		// when input is already visible without being focused.
+		$("#search-input").focus();
+		return false;
+	});
+
+	$("#search-close-btn").click(function() {
+		$(".nav-search-overlay")
+			.removeClass("opened")
+			.one("transitionend webkitTransitionEnd oTransitionEnd", function () {
+				// Clearing the field only after transition has finished to avoid visual
+				// glitch of going back to "Search" placeholder for a fraction of time.
+				// Then trigger `input` event to clear results and bring back content.
+				$("#search-input").val("").trigger("input");
+			});
+		return false;
 	});
 })();
