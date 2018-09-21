a7bebf920264ab1b225e84fda17959a9c7d6daa2
Jérémie Astori <jeremie@astori.fr>
2018-09-21 01:00:35 -0400

    Extract search data into its own file for browser caching

---
+++ js/search_data.js
@@ -0,0 +1,14 @@
+---
+---
+window.store = {
+  {%- assign documents = site.documents | concat: site.pages %}
+  {%- for document in documents %}
+  {%- unless document.title %}{% continue %}{% endunless %}
+  "{{ document.url }}": {
+    "title": "{{ document.title | xml_escape }}",
+    "category": "{{ document.category | xml_escape }}",
+    "content": {{ document.content | newline_to_br | strip_newlines | replace: "<br />", " " | strip_html | jsonify }}
+  }
+  {%- unless forloop.last -%},{%- endunless -%}
+  {%- endfor %}
+};
