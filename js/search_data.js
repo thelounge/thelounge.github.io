4e34cef89392b4890cbc6bffa761af4273dcea15
Jérémie Astori <jeremie@astori.fr>
2018-11-03 18:05:43 -0400

    Make sure "/index" does not appear in side nav and search result links

--- js/search_data.js
+++ js/search_data.js
@@ -4,7 +4,7 @@ window.search_data = {
   {%- assign documents = site.documents | concat: site.pages %}
   {%- for document in documents %}
   {%- unless document.title %}{% continue %}{% endunless %}
-  "{{ document.url | remove: ".html" }}": {
+  "{{ document.url | remove: ".html" | remove: "/index" }}": {
     "title": "{{ document.title | xml_escape }}",
     "category": "{{ document.category | xml_escape }}",
     "content": {{ document.content | newline_to_br | strip_newlines | replace: "<br />", " " | strip_html | jsonify }}
